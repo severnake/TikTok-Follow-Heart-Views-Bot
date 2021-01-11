3eeebfd48f61d0b51ef01acf95a79c5f4080f01b
Alessandro <alessandrogreco@hacari.org>
2021-01-11 22:06:06 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -133,7 +133,7 @@ chrome_options = webdriver.ChromeOptions()
 #chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/home/nonameon/Documenti/Coding/chromedriver',chrome_options=chrome_options) #Change it
+driver = webdriver.Chrome('/path/to/your/chromedriver',chrome_options=chrome_options) #Change it
 
 driver.get("https://vipto.de/")
 
